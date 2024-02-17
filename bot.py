757533f74c5b2959076ae57d08b5687403bc87eb
DextiN <47779849+dikshitd5@users.noreply.github.com>
2024-02-17 20:30:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,3 @@
-#    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
-
-# Import necessary modules
-# Import necessary modules
 # Import necessary modules
 from telethon import TelegramClient, events
 from decouple import config
@@ -30,29 +15,48 @@ print("Starting...")
 APP_ID = config("APP_ID", default=0, cast=int)
 API_HASH = config("API_HASH", default=None, cast=str)
 SESSION = config("SESSION", default="", cast=str)
-FROM_CHANNEL = config("FROM_CHANNEL", default="", cast=str)
-TO_BOT = config("TO_BOT", default="", cast=str)
+FROM_ = config("FROM_CHANNEL", default="", cast=str)
+TO_ = config("TO_CHANNEL", default="", cast=str)
+
+MEDIA_FORWARD_RESPONSE = config("MEDIA_FORWARD_RESPONSE", default="yes").lower()
+
+FROM = [int(i) for i in FROM_.split()]
+TO = [int(i) for i in TO_.split()]
+
 YOUR_ADMIN_USER_ID = config("YOUR_ADMIN_USER_ID", default=0, cast=int)
+BOT_API_KEY = config("BOT_API_KEY", default="", cast=str)
 
 # Initialize Telethon client
 try:
-    client = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    client.start()
-except Exception as e:
-    print(f"ERROR - {e}")
+    steallootdealUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
+    steallootdealUser.start()
+except Exception as ap:
+    print(f"ERROR - {ap}")
     exit(1)
 
 # Event handler for incoming messages
-@client.on(events.NewMessage(chats=FROM_CHANNEL))
-async def forward_to_bot(event):
-    try:
-        # Forward the message to the target bot
-        await client.forward_messages(TO_BOT, event.message)
-        print(f"Forwarded message to bot {TO_BOT}")
-    except Exception as e:
-        print(f"Error forwarding message to bot {TO_BOT}: {e}")
+@steallootdealUser.on(events.NewMessage(incoming=True, chats=FROM))
+async def sender_bH(event):
+    for i in TO:
+        try:
+            message_text = event.raw_text.lower()
 
-# Run the client
+            if event.media:
+                user_response = MEDIA_FORWARD_RESPONSE
+                if user_response != 'yes':
+                    print(f"Media forwarding skipped by user for message: {event.raw_text}")
+                    continue
+
+                await steallootdealUser.send_message(i, message_text, file=event.media)
+                print(f"Forwarded media message to channel {i}")
+
+            else:
+                await steallootdealUser.send_message(i, message_text)
+                print(f"Forwarded text message to channel {i}")
+
+        except Exception as e:
+            print(f"Error forwarding message to channel {i}: {e}")
+
+# Run the bot
 print("Bot has started.")
-client.run_until_disconnected()
-
+steallootdealUser.run_until_disconnected()
